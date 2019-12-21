e9336b99cf8740878dc8e0531813b70d035ddee0
Brittani Rubil <bmb23@njit.edu>
2019-12-20 21:30:39 -0500

    Task: Updated run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
+ from src.app import create_app
 
-from src.app import create_app
+ env_name = os.getenv('FLASK_ENV')
+ app = create_app(env_name)
 
-if __name__ == '__main__':
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
-    app.run()
+ if __name__ == '__main__':
+   port = os.getenv('PORT')
+   # run app
+   app.run(host='0.0.0.0', port=port)
