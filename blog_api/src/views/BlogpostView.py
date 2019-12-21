349a23527ba64995fdf3a4f6410169effd3bfdcb
Brittani Rubil <bmb23@njit.edu>
2019-12-20 20:18:47 -0500

    Feature: Update a blogpost

--- blog_api/src/views/BlogpostView.py
+++ blog_api/src/views/BlogpostView.py
@@ -22,6 +22,7 @@ def create():
   data = blogpost_schema.dump(post).data
   return custom_response(data, 201)
 
+
 @blogpost_api.route('/', methods=['GET'])
 def get_all():
     """
@@ -40,6 +41,29 @@ def get_one(blogpost_id):
   post = BlogpostModel.get_one_blogpost(blogpost_id)
   if not post:
     return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post).data
+  return custom_response(data, 200)
+
+
+@blogpost_api.route('/<int:blogpost_id>', methods=['PUT'])
+@Auth.auth_required
+def update(blogpost_id):
+  """
+  Update A Blogpost
+  """
+  req_data = request.get_json()
+  post = BlogpostModel.get_one_blogpost(blogpost_id)
+  if not post:
+    return custom_response({'error': 'post not found'}, 404)
+  data = blogpost_schema.dump(post).data
+  if data.get('owner_id') != g.user.get('id'):
+    return custom_response({'error': 'permission denied'}, 400)
+
+  data, error = blogpost_schema.load(req_data, partial=True)
+  if error:
+    return custom_response(error, 400)
+  post.update(data)
+
   data = blogpost_schema.dump(post).data
   return custom_response(data, 200)
 
